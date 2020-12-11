cac7bc2ee9049b8bffbfaf902828f46621a6daf0
Vlad Timonin <vlad@xaoctech.com>
2020-12-11 15:43:48 +0300

    Add additional MySQL types: - Tinytext - Mediumtext - Longtext - Double - Tinyint - Smallint - Bigint

--- test_data/schema_mysql.rs
+++ test_data/schema_mysql.rs
@@ -31,3 +31,70 @@ table! {
         updatedAt -> Nullable<Datetime>,
     }
 }
+
+// CREATE TABLE text_types (
+//   `id` INT NOT NULL,
+//   `tinytext` TINYTEXT NOT NULL,
+//   `nullableTinytext` TINYTEXT NULL,
+//   `mediumtext` MEDIUMTEXT NOT NULL,
+//   `nullableMediumtext` MEDIUMTEXT NULL,
+//   `longtext` MEDIUMTEXT NOT NULL,
+//   `nullableLongtext` MEDIUMTEXT NULL,
+//   PRIMARY KEY (`id`));
+table! {
+    text_types (id) {
+        id -> Integer,
+        tinytext -> Tinytext,
+        nullableTinytext -> Nullable<Tinytext>,
+        mediumtext -> Mediumtext,
+        nullableMediumtext -> Nullable<Mediumtext>,
+        longtext -> Mediumtext,
+        nullableLongtext -> Nullable<Mediumtext>,
+    }
+}
+
+// CREATE TABLE numeric_types (
+//   `id` INT NOT NULL,
+//   `double` DOUBLE NOT NULL,
+//   `nullableDouble` DOUBLE NULL,
+//   `tinyint` TINYINT NOT NULL,
+//   `nullableTinyint` TINYINT NULL,
+//   `smallint` SMALLINT NOT NULL,
+//   `nullableSmallint` SMALLINT NULL,
+//   `bigint` BIGINT NOT NULL,
+//   `nullableBigint` BIGINT NULL,
+//   PRIMARY KEY (`id`));
+table! {
+    numeric_types (id) {
+        id -> Integer,
+        double -> Double,
+        nullableDouble -> Nullable<Double>,
+        tinyint -> Tinyint,
+        nullableTinyint -> Nullable<Tinyint>,
+        smallint -> Smallint,
+        nullableSmallint -> Nullable<Smallint>,
+        bigint -> Bigint,
+        nullableBigint -> Nullable<Bigint>,
+    }
+}
+
+// CREATE TABLE unsigned_types (
+//   `id` INT NOT NULL,
+//   `unsignedTinyint` TINYINT UNSIGNED NOT NULL,
+//   `nullableUnsignedTinyint` TINYINT UNSIGNED NULL,
+//   `unsignedSmallint` SMALLINT UNSIGNED NOT NULL,
+//   `nullableUnsignedSmallint` SMALLINT UNSIGNED NULL,
+//   `bigint` BIGINT UNSIGNED NOT NULL,
+//   `nullableBigint` BIGINT UNSIGNED NULL,
+//   PRIMARY KEY (`id`));
+table! {
+    unsigned_types (id) {
+        id -> Integer,
+        unsignedTinyint -> Unsigned<Tinyint>,
+        nullableUnsignedTinyint -> Nullable<Unsigned<Tinyint>>,
+        unsignedSmallint -> Unsigned<Smallint>,
+        nullableUnsignedSmallint -> Nullable<Unsigned<Smallint>>,
+        bigint -> Unsigned<Bigint>,
+        nullableBigint -> Nullable<Unsigned<Bigint>>,
+    }
+}
