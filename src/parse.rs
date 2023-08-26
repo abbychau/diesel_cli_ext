206cd6dbfb91e8de77c120096c9abb1e243c4a80
Guilhem Marion <gmarion@netc.fr>
2023-08-26 18:18:34 +0000

    Use diesel prelude if required

--- src/parse.rs
+++ src/parse.rs
@@ -17,6 +17,7 @@ pub struct ParseOutput {
     pub type_uuid: bool,
     pub type_tz: bool,
     pub type_jsonb: bool,
+    pub diesel_macro_use: bool,
 }
 
 pub fn parse(
@@ -45,7 +46,8 @@ pub fn parse(
         mut type_uuid,
         mut type_tz,
         mut type_jsonb,
-    ) = (false, false, false, false, false, false, false, false);
+        mut diesel_macro_use,
+    ) = (false, false, false, false, false, false, false, false, false);
 
     let mut count: u16 = 0;
     let mut struct_name: String = "".to_string();
@@ -208,6 +210,7 @@ pub fn parse(
                         " ".repeat(indent_depth),
                         vec[0].split('.').last().unwrap()
                     ));
+                    diesel_macro_use = true;
                 } else {
                     // add #[table_name = "name"]
                     str_model.push_str(&format!(
@@ -417,6 +420,7 @@ pub fn parse(
         type_uuid,
         type_tz,
         type_jsonb,
+        diesel_macro_use,
     }
 }
 
